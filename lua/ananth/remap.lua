6aa508684acd16cdae29b6ad2d7c5ccb1cb62d07
Ananth Narayan <narayanananth26@gmail.com>
2026-01-04 12:19:37 +0530

    chore: add remaps to yank to and paste from OS clipboard

--- lua/ananth/remap.lua
+++ lua/ananth/remap.lua
@@ -7,3 +7,8 @@ vim.keymap.set('n', '<leader>ex', vim.cmd.Ex)
 vim.keymap.set("n", "<leader>lz", "<cmd>Lazy<CR>", { desc = "Open Lazy.nvim" })
 vim.keymap.set("n", "<leader>so", ":update<CR> :source<CR>", { desc = "Open Lazy.nvim" })
 vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>p", '"+p')
+vim.keymap.set("n", "<leader>P", '"+P')
+
