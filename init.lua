ae3f13842b98f6048f71a692c2acef64d5a23eb8
Ananth Narayan <narayanananth26@gmail.com>
2026-01-04 12:44:28 +0530

    chore: add yank highlight

--- init.lua
+++ init.lua
@@ -5,3 +5,13 @@ vim.o.relativenumber = true
 vim.o.tabstop = 4
 vim.o.swapfile = false
 vim.o.winborder = "rounded"
+
+-- yank highlight
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('highlight_yank', {}),
+  desc = 'Hightlight selection on yank',
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 150 }
+  end,
+})
--- lua/ananth/plugins/rose-pine.lua
+++ lua/ananth/plugins/rose-pine.lua
@@ -1,20 +1,24 @@
 return {
-  "rose-pine/neovim",
-  name = "rose-pine",
-  config = function()
-    require("rose-pine").setup({
-      disable_background = true,
-    })
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+		require("rose-pine").setup({
+			disable_background = true,
+		})
 
-    vim.cmd("colorscheme rose-pine")
+		vim.cmd("colorscheme rose-pine")
 
-    vim.cmd([[
+		vim.cmd([[
       highlight Normal guibg=NONE ctermbg=NONE
       highlight NormalNC guibg=NONE ctermbg=NONE
       highlight NormalFloat guibg=NONE ctermbg=NONE
       highlight FloatBorder guibg=NONE ctermbg=NONE
       highlight EndOfBuffer guibg=NONE ctermbg=NONE
     ]])
-  end,
-}
 
+		vim.api.nvim_set_hl(0, "IncSearch", {
+			bg = "#FF8A95",
+			fg = "#1A1A1A",
+		})
+	end,
+}
