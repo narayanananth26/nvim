59794d8843d73019a6230f8a0df9d51afe7079c2
Ananth Narayan <narayanananth26@gmail.com>
2026-01-04 14:29:36 +0530

    chore: add nvim-colorizer plugin

---
+++ lua/ananth/plugins/nvim-colorizer.lua
@@ -0,0 +1,13 @@
+
+return {
+  {
+    "norcalli/nvim-colorizer.lua",
+    lazy = false,
+    config = function()
+      require("colorizer").setup({
+        "*",
+        css = { rgb_fn = true },
+      })
+    end,
+  },
+}
