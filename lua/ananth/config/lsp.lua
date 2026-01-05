8d04b9f3839c30d61cc32e7bf68fab6bfec6f126
Ananth Narayan <narayanananth26@gmail.com>
2026-01-05 10:44:39 +0530

    chore: add clangd to ensure_installed in mason config and add 4 space indenting to lsp config for c and cpp files

--- lua/ananth/config/lsp.lua
+++ lua/ananth/config/lsp.lua
@@ -52,6 +52,17 @@ vim.api.nvim_create_autocmd("LspAttach", {
   end,
 })
 
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "c", "cpp", "h", "hpp" },
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+  end,
+})
+
 -- vim.lsp.inlay_hint.enable(true)
 
 local severity = vim.diagnostic.severity
