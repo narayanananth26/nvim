626a4da5f71ceab5378ffe9e0fddf3a2baafc369
Ananth Narayan <narayanananth26@gmail.com>
2026-01-16 09:23:19 +0530

    fix: resolve error messages from init.lua

--- init.lua
+++ init.lua
@@ -5,37 +5,36 @@ vim.o.number = true
 vim.o.relativenumber = true
 vim.o.tabstop = 4
 vim.o.swapfile = false
-vim.o.winborder = "rounded"
 
 -- yank highlight
-vim.api.nvim_create_autocmd('TextYankPost', {
-  group = vim.api.nvim_create_augroup('highlight_yank', {}),
-  desc = 'Hightlight selection on yank',
-  pattern = '*',
-  callback = function()
-    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 150 }
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("highlight_yank", {}),
+	desc = "Hightlight selection on yank",
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 150 })
+	end,
 })
 
 -- cursor colors based on mode
 vim.opt.guicursor = {
-  "n:block-CursorNormal",
-  "v:block-CursorVisual",
-  "i-ci:block-CursorInsert-blinkon1",
-  "r-cr:hor20-CursorReplace",
-  "o:hor50-CursorOperator",
+	"n:block-CursorNormal",
+	"v:block-CursorVisual",
+	"i-ci:block-CursorInsert-blinkon1",
+	"r-cr:hor20-CursorReplace",
+	"o:hor50-CursorOperator",
 }
 
 local function set_cursor_colors()
-  vim.api.nvim_set_hl(0, "CursorNormal", { fg = "#000000", bg = "#FF8A95" })
-  vim.api.nvim_set_hl(0, "CursorInsert", { fg = "#000000", bg = "#82c785" })
-  vim.api.nvim_set_hl(0, "CursorVisual", { fg = "#000000", bg = "#76a8f9" })
+	vim.api.nvim_set_hl(0, "CursorNormal", { fg = "#000000", bg = "#FF8A95" })
+	vim.api.nvim_set_hl(0, "CursorInsert", { fg = "#000000", bg = "#82c785" })
+	vim.api.nvim_set_hl(0, "CursorVisual", { fg = "#000000", bg = "#76a8f9" })
 end
 
 set_cursor_colors()
 
 vim.api.nvim_create_autocmd("ColorScheme", {
-  callback = set_cursor_colors,
+	callback = set_cursor_colors,
 })
 
 -- show commands in statusline
