a418fef77500c4abc45ccfc03fe6dda878b79e7b
Ananth Narayan <narayanananth26@gmail.com>
2026-01-16 09:30:43 +0530

    refactor: update completion mapping to Tab and S-Tab

--- lua/ananth/plugins/lsp/completion.lua
+++ lua/ananth/plugins/lsp/completion.lua
@@ -1,48 +1,61 @@
-
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    lazy = false,
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-    },
-    config = function()
-      local cmp = require("cmp")
+	{
+		"hrsh7th/nvim-cmp",
+		lazy = false,
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+		},
+		config = function()
+			local cmp = require("cmp")
 
-      cmp.setup({
-        sources = {
-          { name = "nvim_lsp" },
-        },
-      })
+			cmp.setup({
+				preselect = cmp.PreselectMode.Item,
+				completion = {
+					completeopt = "menu,menuone",
+				},
+				mapping = {
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						else
+							fallback()
+						end
+					end),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						else
+							fallback()
+						end
+					end),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				},
+				sources = {
+					{ name = "nvim_lsp" },
+				},
+			})
 
-      -- `/` search completion
-      cmp.setup.cmdline("/", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = "buffer" },
-        },
-      })
+			cmp.setup.cmdline("/", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
 
-      -- `:` command completion
-      cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources(
-          {
-            { name = "path" },
-          },
-          {
-            {
-              name = "cmdline",
-              option = {
-                ignore_cmds = { "Man", "!" },
-              },
-            },
-          }
-        ),
-      })
-    end,
-  },
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({ { name = "path" } }, {
+					{
+						name = "cmdline",
+						option = {
+							ignore_cmds = { "Man", "!" },
+						},
+					},
+				}),
+			})
+		end,
+	},
 }
