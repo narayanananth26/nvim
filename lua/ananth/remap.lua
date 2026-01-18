e9d5e10c32dc2a56feb62f7d8ab2c51a7c01e621
Ananth Narayan <narayanananth26@gmail.com>
2026-01-18 15:16:27 +0530

    chore: add remap to toggle between windows

--- lua/ananth/remap.lua
+++ lua/ananth/remap.lua
@@ -31,3 +31,8 @@ vim.keymap.set("n", "<leader>b", ":Neotree toggle<CR>", { silent = true })
 
 -- clear search highlight
 vim.keymap.set("n", "<Esc><Esc>", "<cmd>nohlsearch<CR>", { silent = true })
+
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
