d5ab7cd5b785c11101e391132b83d0979e271432
Ananth Narayan <narayanananth26@gmail.com>
2026-03-26 15:03:17 +0530

    chore: set keepFoldsAcrossSessions to false in nvim-origami

--- lua/ananth/plugins/nvim-origami.lua
+++ lua/ananth/plugins/nvim-origami.lua
@@ -1,7 +1,9 @@
 return {
 	"chrisgrieser/nvim-origami",
+	tag = "v1.9",
 	event = "VeryLazy",
 	opts = {
+		keepFoldsAcrossSessions = false,
 		foldtext = {
 			lineCount = {
 				template = " %d",
