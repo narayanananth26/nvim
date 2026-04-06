0afbd544591d9b9603102371a0118e26b9765bc1
Ananth Narayan <narayanananth26@gmail.com>
2026-04-06 23:03:41 +0530

    chore: add git config to telescope

--- lua/ananth/plugins/telescope.lua
+++ lua/ananth/plugins/telescope.lua
@@ -6,8 +6,17 @@ return {
 			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 			"nvim-telescope/telescope-ui-select.nvim",
 		},
+
 		config = function()
 			local builtin = require("telescope.builtin")
+			local actions = require("telescope.actions")
+			local action_state = require("telescope.actions.state")
+
+			local function show_commit(prompt_bufnr)
+				local selection = action_state.get_selected_entry()
+				actions.close(prompt_bufnr)
+				vim.cmd("Git show " .. selection.value)
+			end
 
 			require("telescope").setup({
 				defaults = {
@@ -32,6 +41,12 @@ return {
 					live_grep = {
 						additional_args = { "--hidden", "--glob", "!.git", "--glob", "!node_modules" },
 					},
+					git_commits = {
+						mappings = {
+							i = { ["<CR>"] = show_commit },
+							n = { ["<CR>"] = show_commit },
+						},
+					},
 				},
 			})
 
@@ -41,6 +56,9 @@ return {
 			vim.keymap.set("n", "<leader>gg", builtin.live_grep, { desc = "Telescope live grep" })
 			vim.keymap.set("n", "<leader>bb", builtin.buffers, { desc = "Telescope buffers" })
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+			vim.keymap.set("n", "<leader>gf", builtin.git_files, { desc = "Git files" })
+			vim.keymap.set("n", "<leader>cc", builtin.git_commits, { desc = "Git commits" })
+			vim.keymap.set("n", "<leader>b", builtin.git_branches, { desc = "Git branches" })
 		end,
 	},
 }
