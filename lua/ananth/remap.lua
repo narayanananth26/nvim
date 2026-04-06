baee16598bc132b3ae3cb8dc7d686507150084cd
Ananth Narayan <narayanananth26@gmail.com>
2026-04-06 22:40:11 +0530

    chore: ask confirmation before quitting last buffer, add keympas for :only and cprev, cnext

--- lua/ananth/remap.lua
+++ lua/ananth/remap.lua
@@ -6,35 +6,25 @@ vim.keymap.set("n", "<leader>w", function()
 	require("conform").format({ lsp_format = "fallback" })
 	vim.cmd("write")
 end, { desc = "Write and format" })
-vim.keymap.set("n", "<leader>q", ":quit<CR>")
+vim.keymap.set("n", "<leader>q", function()
+	local listed = vim.fn.getbufinfo({ buflisted = 1 })
+	if #listed <= 1 and vim.fn.confirm("Quit?", "&Yes\n&No") ~= 1 then return end
+	vim.cmd("quit")
+end, { desc = "Quit (confirm if last buffer)" })
 vim.keymap.set("n", "<leader>z", function()
 	require("conform").format({ lsp_format = "fallback" })
 	vim.cmd("write")
 	vim.cmd("quit")
 end, { desc = "Format, write, and quit" })
 
--- :Ex
-vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
-
--- :Lazy
-vim.keymap.set("n", "<leader>lz", "<cmd>Lazy<CR>", { desc = "Open Lazy.nvim" })
-
 -- source file
 vim.keymap.set("n", "<leader>so", ":update<CR> :source<CR>", { desc = "Open Lazy.nvim" })
-
--- language format
-vim.keymap.set("n", "<leader>lf", function()
-	require("conform").format({ lsp_format = "fallback" })
-end, { desc = "Format buffer" })
 
 -- clipboard keymaps
 vim.keymap.set("n", "<leader>y", '"+y')
 vim.keymap.set("v", "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>p", '"+p')
 vim.keymap.set("n", "<leader>P", '"+P')
-
--- neo-tree toggle
-vim.keymap.set("n", "<leader>b", ":Neotree toggle<CR>", { silent = true })
 
 -- clear search highlight (using <leader><CR> instead of <Esc><Esc> to avoid ESC delay)
 vim.keymap.set("n", "<leader><CR>", "<cmd>nohlsearch<CR>", { silent = true, desc = "Clear search highlight" })
@@ -44,6 +34,7 @@ vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<leader>o", ":only<CR>", { silent = true, desc = "Close all other windows" })
 
 -- buffer navigation (VSCode-like)
 vim.keymap.set("n", "<Tab>", ":bnext<CR>", { silent = true, desc = "Next buffer" })
@@ -75,3 +66,7 @@ vim.cmd([[
 -- git keymaps
 vim.keymap.set("n", "<leader>gs", "<cmd>Git | only<CR>", { desc = "Git Status" })
 vim.keymap.set("n", ",,", "<C-^>", { desc = "Toggle buffers" })
+
+-- quifix
+vim.keymap.set("n", "]q", ":cnext<CR>", { desc = "Quickfix next" })
+vim.keymap.set("n", "[q", ":cprev<CR>", { desc = "Quickfix prev" })
