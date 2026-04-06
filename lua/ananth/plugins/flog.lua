10ff345610abb13875bcf4e0fff5a6a67e5c8633
Ananth Narayan <narayanananth26@gmail.com>
2026-04-06 23:04:29 +0530

    chore: update keymap for git log graph

--- lua/ananth/plugins/flog.lua
+++ lua/ananth/plugins/flog.lua
@@ -4,8 +4,7 @@ return {
 		dependencies = { "tpope/vim-fugitive" },
 		cmd = { "Flog", "Flogsplit", "Floggit" },
 		keys = {
-			{ "<leader>gl", "<cmd>Flog<CR>", desc = "Git log graph" },
-			{ "<leader>gL", "<cmd>Flogsplit<CR>", desc = "Git log split" },
+			{ "<leader>ll", "<cmd>Flog<CR>", desc = "Git log graph" },
 		},
 	},
 }
