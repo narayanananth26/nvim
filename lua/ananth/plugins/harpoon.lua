627b88c8f69379323b00d702fbd22ab89aca4ffd
Ananth Narayan <narayanananth26@gmail.com>
2026-04-18 14:48:16 +0530

    chore: add notification for harpoon add file

--- lua/ananth/plugins/harpoon.lua
+++ lua/ananth/plugins/harpoon.lua
@@ -6,8 +6,11 @@ return {
 		local harpoon = require("harpoon")
 		harpoon:setup()
 
-		vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = "Harpoon add file" })
 		vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon menu" })
+		vim.keymap.set("n", "<leader>ha", function()
+			harpoon:list():add()
+			vim.notify("Harpoon: added " .. vim.fn.expand("%:t"), vim.log.levels.INFO)
+		end, { desc = "Harpoon add file" })
 
 		vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Harpoon file 1" })
 		vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Harpoon file 2" })
