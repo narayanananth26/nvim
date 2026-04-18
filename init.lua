e3483dc4e0ffd80e23831a68d58e64ee4f8564bf
Ananth Narayan <narayanananth26@gmail.com>
2026-04-18 17:26:19 +0530

    refactor: set tabstop to 4

--- init.lua
+++ init.lua
@@ -4,9 +4,9 @@ require("ananth")
 
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-vim.o.softtabstop = 2
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
 vim.o.expandtab = true
 vim.o.swapfile = false
 
