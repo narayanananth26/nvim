50cce733d294538f3d57ea1cce7535a981403f8c
Ananth Narayan <narayanananth26@gmail.com>
2026-04-19 09:59:46 +0530

    chore: remove redundant fugitive config function

--- lua/ananth/plugins/fugitive.lua
+++ lua/ananth/plugins/fugitive.lua
@@ -1,14 +1,5 @@
 return {
 	{
 		"tpope/vim-fugitive",
-		config = function()
-			-- Fugitive-specific keymaps
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = "fugitive",
-				callback = function()
-					vim.keymap.set("n", "P", ":!git push<CR>", { buffer = true, desc = "Git push" })
-				end,
-			})
-		end,
 	},
 }
