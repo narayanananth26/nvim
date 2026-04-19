a5426bf30fdb3f59ef477c047cedd6f4556fb2a2
Ananth Narayan <narayanananth26@gmail.com>
2026-04-19 10:06:55 +0530

    feat: add fugitive config for git push --force

--- lua/ananth/plugins/fugitive.lua
+++ lua/ananth/plugins/fugitive.lua
@@ -1,5 +1,13 @@
 return {
 	{
 		"tpope/vim-fugitive",
+		config = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "fugitive",
+				callback = function()
+					vim.keymap.set("n", "gpf", ":Git push --force<CR>", { desc = "Fugitive git push force" })
+				end,
+			})
+		end,
 	},
 }
