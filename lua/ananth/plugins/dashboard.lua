6450c04b6b72d78ddfbb08212e2e3f4de1088d42
Ananth Narayan <narayanananth26@gmail.com>
2026-05-01 15:51:23 +0530

    chore: add icons for all actions

--- lua/ananth/plugins/dashboard.lua
+++ lua/ananth/plugins/dashboard.lua
@@ -41,17 +41,16 @@ boo bitch!
 					header = vim.split(logo, "\n"),
 					-- stylua: ignore
 					center = {
-						{ action = "Telescope find_files", desc = " Find File", icon = " ", key = "f" },
-						{ action = "ene | startinsert", desc = " New File", icon = " ", key = "n" },
-						{ action = "Telescope oldfiles", desc = " Recent Files", icon = " ", key = "r" },
-						{ action = "Telescope live_grep", desc = " Find Text", icon = " ", key = "g" },
-						{ action = 'lua require("telescope.builtin").find_files({ cwd = vim.fn.stdpath("config") })', desc = " Config", icon = " ", key = "c" },
+						{ action = "Telescope find_files", desc = " Find File", icon = "󰈞 ", key = "f" },
+						{ action = "ene | startinsert", desc = " New File", icon = "󰝒 ", key = "n" },
+						{ action = "Telescope oldfiles", desc = " Recent Files", icon = "󰋚 ", key = "r" },
+						{ action = "Telescope live_grep", desc = " Find Text", icon = "󰱼 ", key = "g" },
 						{ action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "l" },
-						{ action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit", icon = " ", key = "q" },
+						{ action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit", icon = "󰗼 ", key = "q" },
 					},
 					footer = function()
 						---@diagnostic disable-next-line: different-requires
-					local stats = require("lazy").stats()
+						local stats = require("lazy").stats()
 						local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
 						return {
 							"⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms",
