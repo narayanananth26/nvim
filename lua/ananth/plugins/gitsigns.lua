87dbdee6a6ecbe37700481e6c404333ad15c3fa0
Ananth Narayan <narayanananth26@gmail.com>
2026-05-27 09:21:21 +0530

    chore: remove gitsigns keymaps

--- lua/ananth/plugins/gitsigns.lua
+++ lua/ananth/plugins/gitsigns.lua
@@ -2,21 +2,5 @@ return {
 	"lewis6991/gitsigns.nvim",
 	config = function()
 		require("gitsigns").setup()
-
-		-- Hunk staging
-		vim.keymap.set("n", "<leader>hs", ":Gitsigns stage_hunk<CR>", { desc = "Stage hunk" })
-		vim.keymap.set("v", "<leader>hs", ":Gitsigns stage_hunk<CR>", { desc = "Stage selected lines" })
-		vim.keymap.set("n", "<leader>hu", ":Gitsigns undo_stage_hunk<CR>", { desc = "Undo stage hunk" })
-		vim.keymap.set("n", "<leader>hS", ":Gitsigns stage_buffer<CR>", { desc = "Stage entire buffer" })
-		vim.keymap.set("n", "<leader>hr", ":Gitsigns reset_hunk<CR>", { desc = "Reset hunk" })
-		vim.keymap.set("v", "<leader>hr", ":Gitsigns reset_hunk<CR>", { desc = "Reset selected lines" })
-		vim.keymap.set("n", "<leader>hR", ":Gitsigns reset_buffer<CR>", { desc = "Reset entire buffer" })
-
-		-- Hunk navigation
-		vim.keymap.set("n", "]h", ":Gitsigns next_hunk<CR>", { desc = "Next hunk" })
-		vim.keymap.set("n", "[h", ":Gitsigns prev_hunk<CR>", { desc = "Previous hunk" })
-
-		-- Diff view
-		vim.keymap.set("n", "<leader>gd", ":Gitsigns diffthis<CR>", { desc = "Diff this file" })
 	end,
 }
